134a97a122468c7277f59529c696fb732652f3ab
nahblue <ana.paula.alves11@gmail.com>
2023-12-06 18:07:23 -0300

    implementação do render das notas na page home e na page details, funcionalidade de pesquisa

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react'
 import { Container, Links, Content } from './styles'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import { api } from '../../services/api'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
@@ -9,44 +13,89 @@ import { Tag } from '../../components/Tag'
 
 
 export function Details() {
+  const [ data, setData ] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack(){
+    navigate("/")
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?")
+
+    if(confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate("/")
+    }
+  }
+
+  useEffect(() =>{
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
   
   return(
     <Container>
       <Header />
 
-      <main>
-        <Content>
+      {
+        data &&
+        <main>
+          <Content>
 
-          <ButtonText title="Excluir nota"/>
+            <ButtonText 
+              title="Excluir nota"
+              onClick={handleRemove}
+            />
 
-          <h1>
-            Introdução ao React
-          </h1>
+            <h1>
+              {data.title}
+            </h1>
 
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni distinctio excepturi mollitia possimus exercitationem cum. Est voluptatum voluptatem ex quod voluptas pariatur consequuntur magnam odio amet placeat? Impedit, quo dolorem!
-          </p>
+            <p>
+              {data.description}
+            </p>
 
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href='#'>https://www.rocketseat.com.br/</a>
-              </li>
-              <li>
-                <a href='#'>https://www.rocketseat.com.br/</a>
-              </li>
-            </Links>
-          </Section>
+            {
+              data.links &&
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={String(link.id)}>
+                        <a href={link.url} target='_blank'>{link.url}</a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="node" />
-          </Section>
-          
-          <Button title="Voltar"/>
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag 
+                      key={String(tag.id)}
+                      title={tag.name} 
+                    />
+                  ))
+                }
+              </Section>
+            }
+            
+            <Button title="Voltar" onClick={handleBack} />
 
-        </Content>
-      </main>
+          </Content>
+        </main>
+      }
     </Container>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FiPlus, FiSearch } from 'react-icons/fi'
+
+import { api } from '../../services/api'
+
 import { Container, Brand, Menu, Search, Content, NewNote} from './styles'
 
 import { Header } from '../../components/Header'
@@ -8,6 +13,53 @@ import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
 
 export function Home() {
+  const [ tags, setTags ] = useState([])
+  const [ tagsSelected, setTagsSelected ] = useState([])
+  const [ notes, setNotes ] = useState([])
+
+  const [ search, setSearch ] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleTagSelected(tagName) {
+    if(tagName === "all") {
+      return setTagsSelected([])
+    }
+
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if(alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+      
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`)
+  }
+
+  useEffect(() => {
+    async function fetchTags(){
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
+  useEffect(() => {
+    async function fetchNotes(){
+      const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`)
+      setNotes(response.data)
+    }
+
+    fetchNotes()
+  }, [tagsSelected, search])
+
   return(
     <Container>
       <Brand>
@@ -17,29 +69,45 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" isactive /></li>
-        <li><ButtonText title="React"/></li>
-        <li><ButtonText title="Nodejs"/></li>
-        
+        <li>
+          <ButtonText 
+            title="Todos" 
+            onClick={() => handleTagSelected("all")}
+            isactive={tagsSelected.length === 0}
+          />
+        </li>  
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name} 
+                onClick={() => handleTagSelected(tag.name)}
+                isactive={tagsSelected.includes(tag.name)}
+              />
+            </li>        
+          ))
+        }
       </Menu>
 
       <Search>
         <Input 
           placeholder="Pesquisar pelo título"
           icon={FiSearch}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note data={{ 
-            title: 'React', 
-            tags: [
-              { id: '1', name: 'react' },
-              { id: '2', name: 'nodejs' }
-            ]
-          }} 
-          />
+          {
+            notes.map(note => (
+              <Note
+                key={String(note.id)} 
+                data={note} 
+                onClick={() => handleDetails(note.id)}
+              />
+            ))
+          }
         </Section>
       </Content>
 
